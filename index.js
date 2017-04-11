dbd1ee7698a088f33d7e91b5886170f21c6068d0
Trần Đình Trọng <tdtrong056@gmail.com>
2017-04-11 22:52:43 +0700

    update source project

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public')); 
+app.use("/components",express.static('bower_components')); 
+
+//routes
 app.get('/', function(req, res){
-  res.send('hello world');
+  res.send("Hello");
 });
 
+//start server
 app.listen(3000);
