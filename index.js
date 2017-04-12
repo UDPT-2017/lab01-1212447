d16321f9b3959f7ddf461ddec96583a846fc0fb3
Trần Đình Trọng <tdtrong056@gmail.com>
2017-04-13 01:45:45 +0700

    add views and layout main for all view

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 var express = require('express');
+var exphbs  = require('express-handlebars');
+
 var app = express();
 
 app.use(express.static('public')); 
 app.use("/components",express.static('bower_components')); 
 
+app.engine('hbs', exphbs({
+	extname: ".hbs",
+	defaultLayout: 'main'
+}));
+
+app.set('view engine', 'hbs');
+
 //routes
 app.get('/', function(req, res){
-  res.send("Hello");
+  	res.render("index", {title: "Lab01 - Home"});
 });
-
+app.get('/home', function(req, res){
+  	res.render("index", {title: "Lab01 - Home"});
+});
+app.get('/abouts', function(req, res){
+  	res.render("abouts", {title: "Lab01 - Abouts"});
+});
+app.get('/albums', function(req, res){
+  	res.render("albums", {title: "Lab01 - Albums"});
+});app.get('/blog', function(req, res){
+  	res.render("blog", {title: "Lab01 - Blog"});
+});
 //start server
-app.listen(3000);
+app.listen(5000, function(){
+	console.log("Listen in port 5000")}
+);
